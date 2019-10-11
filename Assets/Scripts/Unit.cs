d1de09412fa8964d5cefdb012d50e6668695daca
Mason Tolman <nosam@null.net>
2019-10-11 22:18:52 +0000

    Nearest Enemy Implemented

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -6,4 +6,15 @@ public class Unit : MonoBehaviour
 {
     public bool isMoveable { get; set; }
     public bool isTargetable { get; set; }
+    public bool hasTarget { get; set; }
+    public bool canShoot { get; set; }
+    public float attackCooldown { get; set; }
+
+    void Start()
+    {
+        isTargetable = true;
+        isMoveable = false;
+        canShoot = true;
+        attackCooldown = 1.0f;
+    }
 }
